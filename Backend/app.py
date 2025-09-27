2cd99b65637a31a81f2192c51335ba3644a1db59
Adityachaudhari11 <adityachaudhari9898@gmail.com>
2025-09-27 15:22:08 +0530

    frontend

--- Backend/app.py
+++ Backend/app.py
@@ -14,7 +14,7 @@ app = FastAPI(title="MoSPI AI Survey API")
 
 origins = [
     "https://webcade.vercel.app/",  # <-- Replace with actual Vercel URL
-    "http://localhost:3000",                 # for local testing (optional)
+                     # for local testing (optional)
 ]
 # Enable CORS
 app.add_middleware(
